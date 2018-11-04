85c990d9150c898d5e3d4674c32cfdf5a160e92b
ambidext-PC\ambidext <ambidext@gmail.com>
2018-11-04 21:14:27 +0900

    Finish 1st Step

---
+++ AccountConverter/FormAccountConverter.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AccountConverter
+{
+    partial class FormAccountConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(93, 12);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(453, 21);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Location = new System.Drawing.Point(49, 54);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(31, 12);
+            this.labelUser.TabIndex = 2;
+            this.labelUser.Text = "User";
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(12, 95);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 4;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(93, 51);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(100, 21);
+            this.textBoxUser.TabIndex = 5;
+            // 
+            // FormAccountConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 404);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "FormAccountConverter";
+            this.Text = "Account Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.TextBox textBoxUser;
+    }
+}
+
---
+++ AccountConverter/FormAccountConverter.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AccountConverter
+{
+    public partial class FormAccountConverter : Form
+    {
+        public string m_CurDir = ".";
+        Dictionary<string, string> m_dicItemContents = new Dictionary<string, string>();
+        Dictionary<string, string> m_dicItemStore = new Dictionary<string, string>();
+
+        public FormAccountConverter()
+        {
+            InitializeComponent();
+        }
+
+        public void loadItemFiles()
+        {
+            // load item file
+            FileStream fs = new FileStream(m_CurDir+"\\item_contents.csv", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] words = line.Split(',');
+                m_dicItemContents.Add(words[0], words[1]);
+            }
+            sr.Close();
+            fs.Close();
+
+            fs = new FileStream(m_CurDir+"\\item_store.csv", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            sr = new StreamReader(fs, Encoding.Default);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] words = line.Split(',');
+                m_dicItemStore.Add(words[0], words[1]);
+            }
+            sr.Close();
+            fs.Close();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All File(*.*)|*.*";
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            ofd.Title = "Select a file";
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            textBoxPath.Text = ofd.FileName;
+            m_CurDir = ofd.FileName.Replace(Path.GetFileName(ofd.FileName), "");
+            loadItemFiles();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            if (textBoxUser.Text == "")
+            {
+                MessageBox.Show("Fill User");
+                return;
+            }
+
+            string line;
+            FileStream fs = new FileStream(textBoxPath.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            FileStream fw = new FileStream(m_CurDir+"\\out.csv", FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            StreamWriter sw = new StreamWriter(fw, Encoding.Default);
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line == "")
+                    break;
+                string[] words = line.Split('\t');
+                accountInfo objInfo = new accountInfo(words, textBoxUser.Text, m_dicItemContents, m_dicItemStore);
+
+                sw.Write(objInfo.date); sw.Write(",");
+                sw.Write(objInfo.price + ""); sw.Write(",");
+                sw.Write(objInfo.item); sw.Write(",");
+                sw.Write(objInfo.contents); sw.Write(",");
+                sw.Write(objInfo.store); sw.Write(",");
+                sw.Write(objInfo.user);
+                sw.WriteLine();
+            }
+            sr.Close();
+            fs.Close();
+            sw.Close();
+            fw.Close();
+
+            MessageBox.Show("Done!");
+        }
+    }
+
+    public class accountInfo
+    {
+        public string date;
+        public int price;
+        public string item;
+        public string contents;
+        public string store;
+        public string user;
+        Dictionary<string, string> dicContents;
+        Dictionary<string, string> dicStore;
+        public accountInfo()
+        {
+
+        }
+
+        public accountInfo(string[] str, string username, Dictionary<string, string> dic1, Dictionary<string, string> dic2)
+        {
+            date = str[0];
+            date = date.Replace("년", "-");
+            date = date.Replace("월", "-");
+            date = date.Replace("일", "");
+
+            store = str[1];
+            contents = str[2];
+            price = int.Parse(str[3]);
+            user = username;
+
+            dicContents = dic1;
+            dicStore = dic2;
+
+            item = extractItem();
+        }
+
+        public string extractItem()
+        {
+            foreach (var v in dicContents)
+            {
+                if (contents.Contains(v.Key))
+                {
+                    return v.Value;
+                }
+            }
+
+            foreach (var v in dicStore)
+            {
+                if (store.Contains(v.Key))
+                {
+                    return v.Value;
+                }
+            }
+
+            return "미정";
+        }
+    }
+}
